79e5df9e05cd61f0665e2816c97166e8287aff19
MHUSAN <146316522+MHUSAN@users.noreply.github.com>
2024-01-28 22:39:58 +0500

    Add files via upload

---
+++ 4.3 Js Start/main.js
@@ -0,0 +1,41 @@
+// console.log(10);
+// console.log("10");
+// console.log("matn");
+
+// Number = son axborot turi
+
+// var son = 5;
+// console.log(2);
+// console.log(son);
+
+
+// String - matn axborot turi 
+
+// console.log('2' + 2);
+// console.log(2 + 2);
+
+
+// Boolean - matn axborot turi 
+
+// console.log(true); 1
+// console.log(false); 0
+
+// console.log(true + 2 - 1 + false + false + true + true - 3 + true + false);
+
+
+// var total = 50 + 50;
+// var total2 = 50 + 70;
+// var total3 = 50 + 90;
+
+// console.log(total, total2, total3);
+
+
+// var hi = 'hello';
+// alert(hi);
+// console.log(hi);
+
+
+// var myName = prompt("ismingizni kirting");
+// alert(myName);
+
+
---
+++ 4.4/main.js
@@ -0,0 +1,91 @@
+// var x = 4;
+// console.log(x++); postfix 
+// console.log(x);
+// console.log(++x); prefix
+
+
+// if(true){
+//     console.log('shart bajarildi');
+// }else{
+//     console.log("ish bajarilsi");
+// } 
+
+
+
+// var user = prompt('5 + 4');
+// var answer = 9;
+
+// if(user == answer){
+//     console.log("to'g'ri");
+// }else{
+//     console.log("notog'ri");
+// }
+
+
+
+// if(5 == 4){
+//     console.log("1-shart");
+// }else if(5 == 5){
+//     console.log("2-shart");
+// }else{
+//     console.log("xato");
+// }
+
+
+// var gmail = prompt("gmail parol");
+// var figma = prompt("figma parol");
+
+// if(gmail == figma){
+//     console.log("xush kelibsiz");
+// }else{
+//     console.log("xayir");
+// }
+
+
+// var hi = prompt("Salomlashing");
+
+// if(hi == "Salom" || hi == 'salom'){
+//     console.log("Xush kelibsiz");
+// }else if(hi == "Hello"){
+//     console.log("Welcome");
+// }else if(hi == "Привет"){
+//     console.log("pasholl");
+// }else{
+//     console.log("xayir");
+// }
+
+
+// var son = prompt("5 sonini kirting");
+
+// if(son == 5){
+//     console.log('Barakkala');
+// }else{
+//     console.log("ko'tmisan yaxshilab o'qi");
+// }
+
+
+// var x = +prompt('son - 1');
+// var y = +prompt('son - 2');
+
+// if(x > y){
+//     console.log(x + ' katta ' + y + ' dan');
+// }else if(isNaN(x) || isNaN(y)){
+//     console.log("raqam kirting " + x + ' ' + y);
+// }
+// else if(x < y){
+//     console.log(x + ' kichik ' + y + ' dan');
+// }
+// else if(x == y){
+//     console.log("ikkalasi teng");
+// }else{
+//     console.log('Error');
+// }
+
+
+var son = prompt("son kirting");
+
+if(son % 2 == 0){
+    console.log("juft");
+}else{
+    console.log('toq');
+}
