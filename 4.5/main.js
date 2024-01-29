fd7a41f7c945a90ce84ea22bb5d5f27a37cd91bb
MHUSAN <146316522+MHUSAN@users.noreply.github.com>
2024-01-29 10:54:32 +0500

    Add files via upload

---
+++ 4.5/main.js
@@ -0,0 +1,40 @@
+// let i = 0;
+
+// while(i <= 10){
+//     console.log(i);
+//     i++;
+// }
+
+
+// let i = 10;
+
+// while(i > 0){
+//     console.log(i);
+//     i--;
+// }
+
+
+// let = i = 0;
+
+// do{
+//     console.log(i);
+//     i++;
+// }while(i < 0)
+
+
+// for(var i = 0; i <= 10; i++){
+//     console.log(i);
+// }
+
+
+do{
+    var x = prompt('Son kirting');
+}while(isNaN(x))
+
+let answer = 1;
+
+for(let i = 1; i <= x; i++){
+    answer = answer * i;    
+}
+
+console.log(answer);
