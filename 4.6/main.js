96db5d2d13e7af8444015f7b3c468238fda9ec08
MHUSAN <146316522+MHUSAN@users.noreply.github.com>
2024-02-17 22:46:59 +0500

    Add files via upload

---
+++ 4.6/main.js
@@ -0,0 +1,71 @@
+// var son = prompt("son kirting");
+
+// son % 2 == 0 ? alert('juft') : alert('toq')
+
+// if(son % 2 == 0){
+//     alert('juft')
+// }else{
+//     alert('toq')
+// }
+
+
+// let i = 0;
+
+// for(i = 0; i <= 5; i++){
+//     console.log(i);
+//     if(i == 3) break;   
+// }
+
+// console.log('sik toxtadi i = ' ++ i);
+
+
+
+// var age = 15;
+
+// switch (age) {
+//     case 1:
+//         console.log("1 - qiymat");
+//         break;
+//     case 2:
+//         console.log("2 - qiymat");
+//         break;
+//     case 3:
+//         console.log("3 - qiymat");
+//         break;
+//     default:
+//         console.log("Error");
+// }
+
+
+// function sum(a , b){
+//     return a + b;
+// }
+
+// console.log(sum(15 , 25));
+
+
+// Math.floor() kichik tomonga yaxlitlaydi 
+// Math.ceil() katta tomonga yaxlitlaydi 
+
+
+// function myRandom(){
+//     return Math.floor(Math.random() * 10 +1)
+// }
+
+// console.log(myRandom());
+
+
+// function myRandom(min, max){
+//     return Math.floor(Math.random() * (max + 1 - min)+ min)
+// }
+
+
+// let minSon = +prompt("minimanli son kirit");
+// let maxSon = +prompt("max son kirit");
+
+// for(i = 0; i < 2; i++){
+//     let num1 = myRandom(minSon , maxSon);
+//     let num2 = myRandom(minSon , maxSon);
+
+//     let pr = +prompt(num1 + ' + ' + num2 + ' = ')
+// }
