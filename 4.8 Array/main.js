827c5df3578e2120e985014864c5df33415322d8
MHUSAN <146316522+MHUSAN@users.noreply.github.com>
2024-02-26 21:58:43 +0500

    Add files via upload

---
+++ 4.8 Array/main.js
@@ -0,0 +1,71 @@
+// []
+
+// let num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// console.log(num);
+// console.log(num[5]);
+
+
+// let num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// for(let i = 0; i < num.length; i++){
+//     answer += num[i] % 2 != 0 ? 1 : 0
+//     answer += num[i] % 2 != 0 ? 1 : 0
+// }
+
+
+// let num = [1, 2, true, false, NaN, undefined, Infinity, null, "proweb", { name: "karl" }]
+
+// console.log(num[5]);
+
+
+// for(let i = 0; i < num.length; i++){
+//     console.log(i);
+//     console.log(i,num[i]);
+// }
+
+
+// let str = "Hello World"
+
+// // console.log(str.length);
+
+// for(let i = 0; i < str.length; i++){
+//     console.log(i, str[i]);
+// }
+
+
+// let str = 'Html css js'
+// let str = 'Html css js' .split(' ')
+// let str = 'Html css js'.split(' ').join(' , ')
+
+// console.log(str);
+
+// // split -mattni massifga aylatiriberadi
+
+// // join - mossivni matinga aylatiriberadi 
+
+
+// let users = ['Said' , 'Jahongir' , 'Akmal']
+
+// console.log(users);       
+
+
+// let users = ['Said', 'jahongir', 'Asqar', 'Atham', 'Akmal'];
+
+// console.log(users);
+
+// for (let i = 0; i < users.length; i++) {
+//     if (users[i] == 'Akmal') {
+//         console.log(users[i]);
+//         users.splice(i, 1)
+//         console.log(users);
+//     }
+// }
+
+
+let users = ['Said', 'jahongir'];
+let number = [1, 2, 3, 4, 5];
+
+let array = users.concat(number)
+
+console.log(array);
