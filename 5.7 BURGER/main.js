55e53d5944cf83791faccf896241386433c91cd3
MHUSAN <146316522+MHUSAN@users.noreply.github.com>
2024-03-25 19:07:24 +0500

    Update main.js

--- 5.7 BURGER/main.js
+++ 5.7 BURGER/main.js
@@ -28,9 +28,19 @@ const product = {
         name: 'dburger',
         price: 24000,
         amound: 0,
-        img: './image/image 4                                    .png',
+        img: './image/image 4.png',
         get totalsum() {
             return this.price * this.amound
         },
     },
 }
+
+const 
+productBtns = document.querySelectorAll('.wrapper__bottom-btn'),
+basketBtn = document.querySelector('.wrapper__top-btn'),
+basketindicator = document.querySelector('.wrapper__top-indicator'),
+basketModal = document.querySelector('.basket'),
+closeBasketModal = document.querySelector('.basket__top-btnClose'),
+bsaketChechlist = document.querySelector('..basket__checklist'),
+basketTotalPrice = document.querySelector('.basket__bottom-totalPrice'),
+basketPrint = document.querySelector('.basket__bottom');
